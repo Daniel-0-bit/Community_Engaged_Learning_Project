b044f4fdccceb3083c6fc06ba8b5691add373c0b
DanielB <djb675@msstate.edu>
2026-04-21 14:50:42 -0500

    Development
    Work done on calc_a.py

--- calc_a.py
+++ calc_a.py
@@ -2,18 +2,20 @@ import minor_loss as hmin
 import utility as utl
 import numpy as np
 
+from utility import unit
+
 #Pipe parameters
-length = 100 #ft
-pipe_rough = 0.02 #ft
+length = 100
+pipe_rough = 0.02
 
 #Heat exchangers
-H_x = lambda Q: 0.0049*np.pow(Q,1.852) # Q must be in gallons per minute.  Loss in ft*lbf/lbm
-A_hx = 5000 #ft^2
-U = lambda Q: 1/(1/(13*np.pow(Q,0.8))+0.047) # Q in gpm.  Returns U in BTU/(hr*ft^2*degF)
+H_x = lambda Q: (0.0049*np.pow(Q,1.852)) # Q must be in gallons per minute.  Loss in ft*lbf/lbm
+A_hx = 5000*unit['ft']**2
+U = lambda Q: (1/(1/(13*np.pow(Q,0.8))+0.047)) # Q in gpm.  Returns U in BTU/(hr*ft^2*degF)
 
 # Air temperatures
-T_in = 72 # degF
-T_out = 10 # degF
+T_in = unit['F>R'](72)
+T_out = unit['F>R'](10)
 
 # Water properties
 density = 62.4 #lbm/ft^3
--- utility.py
+++ utility.py
@@ -1,5 +1,8 @@
 import sys
 
+g = 32.174*12 #in/s^2
+g_c = 32.174*12 #lbm*in/lbf/s^2
+
 unit = { # Unit system defined in FLT using lbf, inches, and seconds as base units.  Temperature in degrees Rankine
     'in': 1,
     'ft': 12,
@@ -8,37 +11,24 @@ unit = { # Unit system defined in FLT using lbf, inches, and seconds as base uni
     'degR': 1
 }
 
-weight = 62.4*(unit['lbf']/(unit['ft']**3))
-g = 32.174*unit['ft']
-g_c = 32.174 #lbm*ft/lbf/s^2
-
 unit['lbm'] = unit['lbf']*(g_c/g) # Under earth conditions, lbm should be 1:1 with lbf
 unit['psi'] = unit['lbf']/(unit['in']**2)
-unit['gal'] = 231*unit['ft']**3 # Cubic feet in a gallon
 unit['min'] = 60*unit['s']
+unit['hr'] = 60*unit['min']
+unit['gal'] = 231 # Cubic inches in a gallon
 unit['gpm'] = unit['gal']/unit['min'] # Gallons per minute
 unit['F>R'] = lambda T: T+459.67
 unit['R>F'] = lambda T: T-459.67
+unit['BTU'] = 778.17*unit['ft']*unit['lbf']
 
 def Shutdown(msg: str):
     print(msg)
     print("Shutting down")
     sys.exit(1)
 
-def enforceUnit(a: str):
-    x = -1
-    num: float
-    b = ""
-    if "psi" in a:
-        b = a[0:a.find("psi")]
-        num = float(b)/weight
-    elif "ft" in a:
-        b = a[0:a.find("ft")]
-        num = float(b)*unit['ft']
-    else:
-        Shutdown("Invalid unit detected.")
+def enforceUnit(a: str, u: str):
 
-    return num #Returns in base number format.
+    return #Returns in base number format.
 
 text = "23 ft"
 
