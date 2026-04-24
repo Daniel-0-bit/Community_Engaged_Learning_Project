33df1b161908156392b57fb4abff24e53dfa6d70
DanielB <djb675@msstate.edu>
2026-04-23 21:20:53 -0500

    Part A HX calculations
    Began development of Part A simulation of heat exchangers.  Creating new functions in utility module to be used for finding the effectiveness of the cross-flow exchangers.

--- calc_a.py
+++ calc_a.py
@@ -6,6 +6,10 @@ import utility as utl
 
 from utility import unit
 
+min_flow = 0
+max_flow = 50
+iteration = 1
+
 g = 32.174 #ft/s^2
 
 #Pipe and network parameters
@@ -14,25 +18,29 @@ Rough = 0.02 #ft
 D = 0.1722 #ft
 cross_area = np.pi*(D/2)**2 #square feet
 
-#Heat exchangers
+#Crossflow heat exchangers (mixed-unmixed)
 H_x = lambda Q: (0.0049*np.pow(Q,1.852)) # Q must be in gallons per minute.  Loss in ft*lbf/lbm
 A_hx = 5000 #ft^2
 U = lambda Q: (1/(1/(13*np.pow(Q,0.8))+0.047)) # Q in gpm.  Returns U in BTU/(hr*ft^2*degF)
 
 #Minor loss factor
 f_t = 0.3086/np.log10((Rough/(3.7*D))**1.11)**2
-k_min = 10*hmin.fittings['elb90'](f_t) # Minor loss coefficient
-k_min += 6*hmin.fittings['gate'](f_t) #minor loss coefficient
+k_min = 10*hmin.fittings['elb90'](f_t) # Minor loss coefficient from elbows
+k_min += 6*hmin.fittings['gate'](f_t) #minor loss coefficient from gates
 
 # Air properties
 T_in = unit['F>R'](72) #Temperature converted to degrees Rankine
 T_out = unit['F>R'](10)
 Air_Mass = 25 #lbm/sec
+c_air = 0.240 #BTU/lbm/degF
+capacity_air = Air_Mass*c_air #BTU/sec/degF
+capacity_air *= unit['hr'] #BTU/hr/degF
 
 # Water properties
 Density_w = 62.4 #lbm/ft^3
 Mu = 0.88e-3 #dynamic viscosity lbm/ft/s
 Nu = 1.4e-5 #kinematic viscosity ft^2/s
+c_w = 1.00 #BTU/lbm/degF
 
 w_mass = [] #lbm/s
 h_maj =[]
@@ -40,8 +48,9 @@ h_min = []
 h_hx = []
 h_total = []
 
-w_mass = np.linspace(0,62.4,100)
+w_mass = np.linspace(min_flow,max_flow,iteration)
 
+#Flow iteration
 for i,q in enumerate(w_mass):
     w_flow = q/Density_w # ft^3/s
     w_flow_gpm = w_flow*(unit['ft']**3/unit['gpm']) #GPM
@@ -50,6 +59,7 @@ for i,q in enumerate(w_mass):
 
     Re = u_w*D/Nu
 
+    ## Head losses
     #Major loss
     if Re <= 1e-8:
         f = 0
@@ -58,21 +68,38 @@ for i,q in enumerate(w_mass):
     else:
         f = 0.3086/(np.log10(6.9/Re+np.pow((Rough/(3.7*D)),1.11))**2)
     
-    h_maj.append(f*(L/D)*(u_w**2/2/utl.g_c))
+    h_maj.append(f*(L/D)*(u_w**2/(2*utl.g_c)))
 
     #Minor loss
-    h_min.append(k_min*(u_w**2/2/utl.g_c))
+    h_min.append(k_min*(u_w**2/(2*utl.g_c)))
 
     #HX loss
     h_hx.append(H_x(w_flow_gpm))
 
     h_total.append(h_maj[i]+h_min[i]+h_hx[i])
 
+    ## HX Rating
+    if q <= 1e-8:
+        continue
+    
+    capacity_w = c_w*q*unit['hr']
+    capacity = sorted([capacity_w, capacity_air])
+    Cr = capacity[0]/capacity[1]
+    NTU = U(w_flow_gpm)*A_hx/capacity[0] #unitless.  capacity[0] is C_min
+    eff: float
+
+    if capacity_air > capacity_w:
+        eff = (1/Cr)*(1-np.exp(-Cr*(1-np.exp(-NTU))))
+    else:
+        eff = 1-np.exp(-(1/Cr)*(1-np.exp(-Cr*NTU)))
+
+    
+
 #Plot rendering
 plt.plot(w_mass,h_maj)
 plt.plot(w_mass,h_min)
 plt.plot(w_mass,h_hx)
-plt.plot(w_mass,h_total)
+plt.plot(w_mass,h_total, lw=2)
 
 plt.title("Head losses")
 plt.xlabel("Mass flow rate (lbm/s)")
--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 g_c = 32.174*12 #lbm*in/lbf/s^2
 
@@ -14,13 +15,13 @@ unit = { # Unit system defined in FLT using lbf, inches, and seconds as base uni
 unit['psi'] = unit['lbf']/(unit['in']**2)
 unit['min'] = 60*unit['s']
 unit['hr'] = 60*unit['min']
-unit['gal'] = 231 # Cubic inches in a gallon
+unit['gal'] = 231*unit['in']**3 # Cubic inches in a gallon
 unit['gpm'] = unit['gal']/unit['min'] # Gallons per minute
 unit['F>R'] = lambda T: T+459.67
 unit['R>F'] = lambda T: T-459.67
 unit['BTU'] = 778.17*unit['ft']*unit['lbf']
 
-def Shutdown(msg: str):
+def Shutdown(msg: str = 'Unknown error'):
     print(msg)
     print("Shutting down")
     sys.exit(1)
@@ -28,3 +29,30 @@ def Shutdown(msg: str):
 def enforceUnit(a: str, u: str):
 
     return #Returns in base number format.
+
+def heatCapacity(a: float,b: float):
+    C = []
+    Cr: float
+    if a<b:
+        C = [a,b]
+    else:
+        C = [b,a]
+
+    Cr = C[0]/C[1]
+
+    return C,Cr
+
+def eff_crossflow(cr: float, ntu: float, condition: str = "neither"):
+    method = {
+        "neither": lambda cr, ntu: 1-np.exp((1/cr)*np.pow(ntu,0.22)*(np.exp(-cr*np.pow(ntu,0.78))-1)),
+
+        "min": lambda cr, ntu: 1-np.exp(-(1/cr)*(1-np.exp(-cr*ntu))),
+
+        "max": lambda cr, ntu: (1/cr)*(1-np.exp(-cr*(1-np.exp(-ntu))))
+    }
+    
+    return method[condition](cr,ntu)
+
+def effectiveness(c: list, btu: float, hx: str):
+    eff: float
+    return eff
